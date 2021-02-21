65ae83fd4f68d6c526921b524575296355e485d5
ayazwani <wani.ayazashraf@gmail.com>
2021-02-21 17:21:36 +0530

    new commit to add comment

--- first.py
+++ first.py
@@ -4,11 +4,15 @@ print("first line of code by gufran ")
 print("second line on dev branch")
 # dev check comment
 # adding comment to test dev commit
-=======
 
 
 #######################################
 
 print("this is from ayaz, hello world")
+
+#this is the new commwwnt to create conflict
+
+
+
 
 
