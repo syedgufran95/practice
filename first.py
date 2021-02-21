c6de2fd93af0d66fff295f7848b41d6bcd4018e8
ayazwani <wani.ayazashraf@gmail.com>
2021-02-21 17:39:59 +0530

    added new changes

--- first.py
+++ first.py
@@ -1,6 +1,5 @@
 print("first line of code by gufran ")
 
-
 print("second line on dev branch")
 # dev check comment
 # adding comment to test dev commit
@@ -19,4 +18,6 @@ print("this is from ayaz, hello world")
 
 #creating conflict now
 
+
+#helllloooooo this is gufran
 
