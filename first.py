e224d8247ad6abeef0050286c270fcb89b5e4292
ayazwani <wani.ayazashraf@gmail.com>
2021-03-15 11:08:21 +0530

    new changes by ayaz

--- first.py
+++ first.py
@@ -1,20 +1,23 @@
 print("first line of code by gufran ")
 
+<<<<<<< HEAD
 print("second line on dev branch")
 # dev check comment
 # adding comment to test dev commit
 
 
-#######################################
 
 print("this is from ayaz, hello world")
 
 #this is the new commwwnt to create conflict
 
 
-###############################
 
 #creating conflict now
+
+
+#helllloooooo this is gufran
+
 
 
 #helllloooooo this is gufran
